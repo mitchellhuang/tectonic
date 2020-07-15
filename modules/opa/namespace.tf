09453eae5e2be5f948ff03e685aa8880574aeddb
Mitchell Huang <mitchell@gingerlabs.com>
2020-07-14 21:25:36 -0700

    Add OPA ignore label to opa and kube-system namespaces

--- modules/opa/namespace.tf
+++ modules/opa/namespace.tf
@@ -1,5 +1,17 @@
 data "kubernetes_namespace" "opa_namespace" {
   metadata {
     name = "opa"
+    labels = {
+      "openpolicyagent.org/webhook" = "ignore"
+    }
+  }
+}
+
+data "kubernetes_namespace" "kube_system_namespace" {
+  metadata {
+    name = "kube-system"
+    labels = {
+      "openpolicyagent.org/webhook" = "ignore"
+    }
   }
 }
